cb59018dc84df5a6ab143f93424a3008a2ba1460
Deivison Cardoso <deivisondc@gmail.com>
2019-10-02 20:56:01 -0300

    first route and model (User)

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
