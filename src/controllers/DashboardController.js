7204ecf4062d68650003e3a95ab3514df86d17fa
Deivison Cardoso <deivisondc@gmail.com>
2019-10-02 21:35:13 -0300

    Dashboard route and controller

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,11 @@
+const Spot = require('../models/Spot');
+
+module.exports = {
+  async show(req, res) {
+    const userId = req.headers.user_id;
+
+    const spots = await Spot.find({ user: userId });
+
+    return res.json({ spots });
+  },
+};
