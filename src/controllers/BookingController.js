a381e2ffbb6a086f077c8a38c6065af58dd6b447
Deivison Cardoso <deivisondc@gmail.com>
2019-10-02 21:43:33 -0300

    Booking route, model and controller

---
+++ src/controllers/BookingController.js
@@ -0,0 +1,19 @@
+const Booking = require('../models/Booking');
+
+module.exports = {
+  async store(req, res) {
+    const userId = req.headers.user_id;
+    const spotId = req.params.spot_id;
+    const { date } = req.body;
+
+    const booking = await Booking.create({
+      date,
+      user: userId,
+      spot: spotId,
+    });
+
+    await booking.populate('user').populate('spot').execPopulate();
+
+    return res.json({ booking });
+  },
+};
