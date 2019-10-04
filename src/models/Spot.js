db97d112fc85780ca0f927c5e33137b7f07ea1a6
Deivison Cardoso <deivisondc@gmail.com>
2019-10-03 21:25:57 -0300

    added virtual field on Spot

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -9,6 +9,14 @@ const SpotSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
   },
+}, {
+  toJSON: {
+    virtuals: true,
+  },
+});
+
+SpotSchema.virtual('thumbmail_url').get(function () {
+  return `http://localhost:3333/files/${this.thumbmail}`;
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
